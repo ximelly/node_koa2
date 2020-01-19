a99e3010b2652178fd1c4a00df8dcf76baad196d
Ximelly <ximelly@163.com>
2020-01-19 18:05:19 +0800

    使用readline
    使用readline

--- utils/add_admin.js
+++ utils/add_admin.js
@@ -1,14 +1,23 @@
 const password=require("../libs/password");
 const config=require("../config");
+const readLine=require("../libs/readLine");
 
 (async()=>{
     let db=await require("../libs/mysql");
-    let name="ximelly",pass="123";
-    let rows=await db.query(`SELECT * FROM ${config.db_table_car} WHERE username=?`,[name]);
-    if(rows.length>0){
-        console.log(`${name}用户已存在`);
-    }else{
-        await db.query(`INSERT INTO ${config.db_table_car} (username,password) VALUES(?,?)`,[name,password(pass)]);
-        console.log(`${name}用户添加成功`);
+    while(1){
+        let name=await readLine.questionAsync("请输入用户名：");
+        if(!name){
+            break;
+        }
+        let rows=await db.query(`SELECT * FROM ${config.db_table_car} WHERE username=?`,[name]);
+        if(rows.length>0){
+            console.log(`${name}用户已存在`);
+        }else{
+            let pass=await readLine.questionAsync("请输入密码：");
+            await db.query(`INSERT INTO ${config.db_table_car} (username,password) VALUES(?,?)`,[name,password(pass)]);
+            console.log(`${name}用户添加成功`);
+        }
     }
+    readLine.close();
+    console.log("readline 已关闭");
 })()
