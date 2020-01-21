f0258e8a2a04ba0d867cf9ae6b7de4c6a8f8f4d8
Ximelly <ximelly@163.com>
2020-01-21 11:46:40 +0800

    独立出banner和login
    独立出banner和login

---
+++ router/admin/banner.js
@@ -0,0 +1,5 @@
+module.exports=(router)=>{
+    router.get("/banner",ctx=>{
+        ctx.body="banner";
+    });
+}
--- router/admin/index.js
+++ router/admin/index.js
@@ -1,8 +1,4 @@
 const Router = require("koa-router");
-const pass = require("../../libs/password");
-const {post} = require("../../libs/body");
-const config = require("../../config");
-const {admin} = require("../../libs/reg");
 
 let router = new Router();
 router.use(async(ctx,next)=>{
@@ -15,44 +11,17 @@ router.use(async(ctx,next)=>{
     }
 })
 
+//引入登录相关路由
+require("./login")(router);
+
+//引入banner相关路由
+require("./banner")(router);
+
 router.get("/",ctx=>{
     ctx.body="登录成功";
 });
 router.get("/car",ctx=>{
     ctx.body="car";
-});
-router.get("/banner",ctx=>{
-    ctx.body="banner";
-});
-router.get("/login",async ctx=>{
-    await ctx.render("admin/login",{error:null,username:null,password:null});
-});
-
-router.post("/login",post(),async ctx=>{
-    let {username,password}=ctx.request.fields;
-    username=username.toLowerCase();
-
-    async function render(msg){
-        await ctx.render("admin/login",{error:msg,username,password});
-    }
-    //数据校验
-    if(!admin.username.test(username)){
-        await render("用户名格式不对，必须4-32为字母或汉字");
-    }else if(!admin.password.test(password)){
-        await render("密码格式不对，必须6-32为字母或汉字");
-    }else{
-        let rows=await ctx.db.query(`SELECT ID,password FROM ${config.db_table_car} WHERE username=?`,[username]);
-        if(rows.length==0){
-            await render(`${username}用户不存在`);
-        }else{
-            if(rows[0].password==pass(password)){
-                ctx.session["adminID"]=rows[0].ID;
-                ctx.redirect("/admin");
-            }else{
-                await render(`用户名或密码错误`);
-            }
-        }
-    }
 });
 
 module.exports=router.routes();
