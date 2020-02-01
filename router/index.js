6244ed239239622462f6f228024517e7b60f1d4e
Ximelly <ximelly@163.com>
2020-02-01 19:48:41 +0800

    动态banner
    动态banner

--- router/index.js
+++ router/index.js
@@ -2,12 +2,19 @@
 const Router = require("koa-router");
 const body=require("../libs/body");
 const static = require("./static");
+const path = require("path");
+const fs = require("promise-fs");
+const {uploadDir} = require("../config");
 
 let router = new Router();
 
 router.use("",require("./web"));
 router.use("/admin",require("./admin"));
 router.use("/api",require("./api"));
+router.use("/upload/:img",async ctx=>{
+    let {img}=ctx.params;
+    ctx.body=fs.createReadStream(path.resolve(uploadDir,img));
+});
 static(router);
 
 //一下两个router用于测试post请求
--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,24 @@
-(async ()=>{
-  let {ok, data}=await $.ajax({
-    url: '/api/banner',
-    dataType: 'json'
-  });
-
-  if(ok){
-    data.forEach(banner=>{
-      console.log(banner);
-      $(`
-      <li>
-        <img src="http://localhost:8080/upload/${banner.image}" alt="">
-        <div class="caption">
-          <h1>价格准 拿钱快</h1>
-          <span>保卖卖车为何足够省心</span>
-        </div>
-      </li>`).appendTo('ul.rslides');
-
-      $('ul.rslides li:eq(0)').css({
-        opacity:1,
-        display:'block'
-      });
-    });
-  }
-
-})();
+(async function(){
+  $.ajax({
+    url:'/api/banner',
+    dataType:'json',
+    success:function(response){
+      let {ok,data}=response;
+      if(ok){
+        data.forEach(banner => {
+          $(`<li>
+              <img src="http://localhost:8081/upload/${banner.image}" alt="">
+              <div class="caption">
+                <h1>${banner.title}</h1>
+                <span>${banner.sub_title}</span>
+              </div>
+            </li>`).appendTo('ul.rslides');
+        });
+        $('ul.rslides li:eq(0)').css({
+          opacity:1,
+          display:'block'
+        });
+      }
+    }
+  })
+})()
