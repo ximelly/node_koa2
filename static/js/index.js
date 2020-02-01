929558ff7b17b8cdd2e4036d15481614189c6cd5
Ximelly <ximelly@163.com>
2020-02-01 22:03:56 +0800

    处理精选
    处理精选

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,74 @@
 (async function(){
-  let datas=await $.ajax({
-    url:'/api/banner',
-    dataType:'json'
-  })
-  let {ok,data}=datas;
-  if(ok){
-    data.forEach(banner => {
-      $(`<li>
-          <img src="http://localhost:8081/upload/${banner.image}" alt="">
-          <div class="caption">
-            <h1>${banner.title}</h1>
-            <span>${banner.sub_title}</span>
-          </div>
-        </li>`).appendTo('ul.rslides');
-    });
-    $('ul.rslides li:eq(0)').css({
-      opacity:1
-    });
+  //banner
+  {
+    let datas=await $.ajax({
+      url:'/api/banner',
+      dataType:'json'
+    })
+    let {ok,data}=datas;
+    if(ok){
+      data.forEach(banner=> {
+        $(`<li>
+            <img src="http://localhost:8081/upload/${banner.image}" alt="">
+            <div class="caption">
+              <h1>${banner.title}</h1>
+              <span>${banner.sub_title}</span>
+            </div>
+          </li>`).appendTo('ul.rslides');
+      });
+      $('ul.rslides li:eq(0)').css({
+        opacity:1
+      });
+    }
+  
+    //翻页
+    let active=0;
+    $(".slider .prev").click(function(){
+      active=active==0?data.length-1:--active;
+      $('ul.rslides li').stop().animate({opacity:0}).eq(active).stop().animate({opacity:1})
+    })
+    $(".slider .next").click(function(){
+      active=active==data.length-1?0:++active;
+      $('ul.rslides li').stop().animate({opacity:0}).eq(active).stop().animate({opacity:1})
+    })
   }
 
-  //翻页
-  let active=0;
-  $(".slider .prev").click(function(){
-    active=active==0?data.length-1:--active;
-    $('ul.rslides li').stop().animate({opacity:0}).eq(active).stop().animate({opacity:1})
-  })
-  $(".slider .next").click(function(){
-    active=active==data.length-1?0:++active;
-    $('ul.rslides li').stop().animate({opacity:0}).eq(active).stop().animate({opacity:1})
-  })
+  //精选
+  {
+    let datas=await $.ajax({
+      url:'/api/choosecar',
+      dataType:'json'
+    })
+    let {ok,data}=datas;
+    if(ok){
+      console.log(data);
+      $("#choosecar").html("");
+      for(let i=0,len=data.length/2;i<len;i++){
+        let arr=data.slice(i*2,i*2+2);
+        $(`<div class="col-1-3">
+            <div class="wrap-col"></div>
+          </div>`).appendTo('#choosecar');
+          arr.forEach((item) => {
+            $(`<div class="item t-center">
+            <div class="item-container">
+              <a href="single.html">
+                <div class="item-caption">
+                  <div class="item-caption-inner">
+                    <div class="item-caption-inner1">
+                      <span>${item.time} / ${item.mileage} / ${item.displace} / ${item.transmission} /  ${item.type}</span>
+                    </div>
+                  </div>
+                </div>
+                <img src='/upload/${item.image}' />
+              </a>
+            </div>
+            <div class="item-info">
+              <a href="single.html"><h3>${item.title}</h3></a>
+              <p>${item.mileage} ${(item.price/1000).toFixed(1)}万</p>
+            </div>
+          </div>`).appendTo('#choosecar .wrap-col:last');
+          });
+      }
+    }
+  }
 })()
